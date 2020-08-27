4eb93490361d052d518dcd38fa74c99740227969
learn1 <69293038+shalab286@users.noreply.github.com>
2020-08-27 16:57:15 +0530

    Update i.py

--- i.py
+++ i.py
@@ -1,7 +1,14 @@
+# excersize 1 general interest 
+
+
 t  = int(input("what is the  time (in year):  "))
 p  = int(input("what is the principle (in rupee):  "))
 r  = int(input("what is the rate (in annual): "))
+
+
 i = p*r/100*t
 i2 = round(i)
+
+
 result = "This is general inter {}"
 print(result.format(i2))
